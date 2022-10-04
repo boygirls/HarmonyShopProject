b3981a702d79df792133d7b1bc65fa2370606741
master <2276284591@qq.com>
2022-10-04 20:09:10 +0800

    构建购物车虚拟数据

--- entry/src/main/java/com/shop/slice/TabListSlice.java
+++ entry/src/main/java/com/shop/slice/TabListSlice.java
@@ -2,10 +2,13 @@ package com.shop.slice;
 
 
 import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
 import com.shop.ResourceTable;
 import com.shop.TabListProvider;
 import com.shop.home.adapter.IndexImagePageSliderProvider;
+import com.shop.home.model.GoodsBean;
 import com.shop.home.model.ResultBeanData;
+import com.shop.type.model.TypeBean;
 import com.shop.util.Constants;
 import com.shop.util.LoadUrlImageUtils;
 import com.zhy.http.okhttp.OkHttpUtils;
@@ -27,10 +30,17 @@ public class TabListSlice extends AbilitySlice {
     private static final HiLogLabel LABEL = new HiLogLabel(HiLog.LOG_APP, 0x00201, "MY_TAG");
     private TabList tabList;
     private PageSlider pageSlider;
+
+    // 分类页面
+    private List<TypeBean.ResultBean> result;
     /**
      * 返回的数据
      */
     private ResultBeanData.ResultBean resultBean;
+
+
+    // 临时数据
+    private  List<GoodsBean> goodsShopCart;
 
     @Override
     public void onStart(Intent intent) {
@@ -43,7 +53,20 @@ public class TabListSlice extends AbilitySlice {
         tabList.selectTabAt(0);
         initIndex(pageSlider);
 
-        HiLog.warn(LABEL, "Failed to visit %{private}s, reason:%{public}d.");
+        initSHopcartData();
+    }
+
+    private void initSHopcartData() {
+
+        for ( int i = 0; i < 8; i++){
+            GoodsBean goodsBean = new GoodsBean();
+            goodsBean.setProduct_id(i + "");
+            goodsBean.setCover_price("123");
+            goodsBean.setFigure("https://cn.bing.com/images/search?view=detailV2&ccid=tDXT8m%2f5&id=19CFDD45FD858956BD09FD8ABECDA1169A1C8281&thid=OIP.tDXT8m_5oTMDAzXQU2k8wQHaHa&mediaurl=https%3a%2f%2fdemosc.chinaz.net%2fFiles%2fpic%2ficons%2f5949%2fq4.png&exph=512&expw=512&q=qq&simid=608037094656079600&FORM=IRPRST&ck=47E85E4F3F8533349B8504EAE7EB0907&selectedIndex=16");
+            goodsBean.setName("2");
+            goodsShopCart.add(goodsBean);
+        }
+
     }
 
     private void initComponent() {
@@ -209,6 +232,10 @@ public class TabListSlice extends AbilitySlice {
     }
     /*初始化购物车列表*/
     private void initShopCart(PageSlider slider) {
+
+
+
+
         //绑定“结算”按钮的点击事件
         Button btn = (Button) pageSlider.findComponentById(ResourceTable.Id_account_button);
         btn.setClickedListener(component -> {
@@ -220,12 +247,67 @@ public class TabListSlice extends AbilitySlice {
     private void initUser(PageSlider slider) {
     }
 
+
+    private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品", "办公文具", "数码周边", "游戏专区"};
+    /**
+     * 分类页面初始化
+     * @param slider
+     */
     private void initType(PageSlider slider) {
+        getUITaskDispatcher().asyncDispatch(() -> {
+            TableLayout cont = (TableLayout) findComponentById(ResourceTable.Id_type_Container);
+            for (int i = 0; i <titles.length; i++) {
+                DirectionalLayout cpt = (DirectionalLayout) LayoutScatter.getInstance(this).parse(ResourceTable.Layout_type_text, null, false);
+                Text text = (Text) cpt.findComponentById(ResourceTable.Id_item_index);
+                text.setText(titles[i]);
+                cont.addComponent(cpt);
+            }
+        });
+
+
+        getGlobalTaskDispatcher(TaskPriority.DEFAULT).asyncDispatch(()->{
+            OkHttpUtils
+                    .get()
+                    .url(Constants.SKIRT_URL)
+                    .id(100)
+                    .build()
+                    .execute(new StringCallback() {
+                        @Override
+                        public void onError(Call call, Exception e, int i) {
+                            HiLog.info(LABEL, "请求失败" + e.toString());
+                        }
+                        @Override
+                        public void onResponse(String response, int id) {
+                            //两位请求成功
+                            switch (id) {
+                                case 100:
+                                    if (response != null) {
+                                        //解析数据
+                                        Gson gson = new Gson();
+                                        TypeBean typeBean = gson.fromJson(response, TypeBean.class);
+                                        result = typeBean.getResult();
+                                        // 处理类型页得到的数据
+                                        typeProcessData();
+                                    }
+                                    break;
+                                case 101:
+                                    HiLog.info(LABEL,"type页面初始化有问题");
+                                    break;
+                            }
+                        }
+                    });
+        });
+    }
+
+    /**
+     * 处理类型页面得到的数据
+     */
+    private void typeProcessData() {
     }
 
 
     /**
-     * 处理请求到的数据
+     * 处理首页请求到的数据
      */
     private void processData() {
 
---
+++ entry/src/main/java/com/shop/type/model/ContainItem.java
@@ -0,0 +1,15 @@
+package com.shop.type.model;
+
+public class ContainItem {
+
+    private String name;
+    public ContainItem(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
