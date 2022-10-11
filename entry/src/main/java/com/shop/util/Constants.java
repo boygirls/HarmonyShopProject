72af299fbd90f1cc633f51051d283fa49d51c669
boygirls <62797482+boygirls@users.noreply.github.com>
2022-10-12 02:18:53 +0800

    Update Constants.java
    更改冲突的网址

--- entry/src/main/java/com/shop/util/Constants.java
+++ entry/src/main/java/com/shop/util/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     //public static String BASE = "http://10.61.80.195:8081";
     //public static String BASE = "http://192.168.43.19:8081";
     public static String BASE = "http://arrm3f.natappfree.cc";
+    
 
     // 请求Json数据基本URL
     public static final String BASE_URL_JSON = BASE+"/AndroidShop/json/";
